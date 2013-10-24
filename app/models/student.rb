38117db4c1a1b143c258fd507934969cfd2f16ab
Daniel Kimbel <dkkimbel@gmail.com>
2013-10-23 18:09:40 -0700

    Implemented and tested change migration method; implemented Student class, but has yet to pass tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,24 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  attr_reader :birthday, :first_name, :last_name
+
+  def initialize(args)
+  	@first_name = args[:first_name]
+  	@last_name = args[:last_name]
+  	@gender = args[:gender]
+  	@birthday = args[:birthday]
+  	@email = args[:email]
+  	@phone = args[:phone]
+  end
+
+  def name
+  	"#{first_name} #{last_name}"
+  end
+
+  def age
+    (Time.new.to_date - birthday).to_i / 365
+  end
 end
