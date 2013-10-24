74e945410f22728a9ab8b852ee5a53837ea18ed5
Daniel Kimbel <dkkimbel@gmail.com>
2013-10-24 11:19:42 -0700

    Implemented validations; still one mysterious failure on email uniqueness

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,16 +2,20 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 
+  validates :email, :uniqueness => true, :format => { :with => /.+@.+\..{2,}/, :message => 'No good!'}
+  validates :age, :numericality => { :greater_than_or_equal_to => 5 }
+  validates :phone_number_getter, :length => { :minimum => 10 }
+
   attr_accessor :first_name, :last_name, :gender, :birthday, :email, :phone
 
-  def initialize(args = {})
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
-    @gender = args[:gender]
-    @birthday = args[:birthday]
-    @email = args[:email]
-    @phone = args[:phone]
-  end
+  # def initialize(args = {})
+  #   @first_name = args.fetch[:first_name]
+  #   @last_name = args[:last_name]
+  #   @gender = args[:gender]
+  #   @birthday = args[:birthday]
+  #   @email = args[:email]
+  #   @phone = args[:phone]
+  # end
 
   def name
     "#{first_name} #{last_name}"
@@ -19,5 +23,9 @@ class Student < ActiveRecord::Base
 
   def age
     (Time.new.to_date - birthday).to_i / 365
+  end
+
+  def phone_number_getter
+    @phone.scan(/\d/).join
   end
 end
